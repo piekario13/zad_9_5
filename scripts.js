7a9be3ce1cf2ca0a64f9e8467c1396da4c9177cc
piekario13 <piekario13@gmail.com>
2017-11-05 18:59:51 +0100

    nie wiem o co tu chodzi :)

---
+++ scripts.js
@@ -0,0 +1,5 @@
+var withButtonClass = document.getElementsByClassName('button');
+console.log(withButtonClass);
+for (var i = 0; i < withButtonClass.length; i++) {
+	console.log(withButtonClass[i].innerText)
+}
